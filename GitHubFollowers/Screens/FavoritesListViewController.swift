6d59b0e8d8a8fa1eb45e1807223419567423847c
Julia <julia.gurbanova@gmail.com>
2024-01-15 18:32:59 +0300

    initial commit

---
+++ GitHubFollowers/Screens/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListViewController.swift
+//  GitHubFollowers
+//
+//  Created by Julia Gurbanova on 15.01.2024.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FollowerListViewController.swift
+//  GitHubFollowers
+//
+//  Created by Julia Gurbanova on 15.01.2024.
+//
+
+import UIKit
+
+class FollowerListViewController: UIViewController {
+
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+}
